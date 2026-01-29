5697550ac9470ad1038c1d9808ce07c3e6d6c612
JUANI\juani <juanisabbatini@gmail.com>
2026-01-29 18:29:41 +0100

    feat(api): add global config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClaimsModule } from './claims/claims.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/claims_db',
+    ),
+
+    ClaimsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
