7528ddd0d449888cd23c2318e75c629d71403b54
JUANI\juani <juanisabbatini@gmail.com>
2026-01-29 20:29:40 +0100

    feat(tests): add jest config

---
+++ apps/api/src/jest.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.(t|j)s',
+    '!src/**/*.module.ts',
+    '!src/**/*.dto.ts',
+    '!src/**/*.entity.ts',
+    '!src/main.ts',
+    '!src/**/index.ts',
+  ],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+};
+
+export default config;
