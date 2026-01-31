9021ce48d326b2f75e00af9e8ed5c9082430309c
JUANI\juani <juanisabbatini@gmail.com>
2026-01-31 12:55:28 +0100

    chore: include production environment for evaluation purposes

---
+++ apps/client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// Se fuerza la subida de este archivo para producción para simplicidad en la evaluación de la prueba técnica.
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:3000/api/v1',
+};
